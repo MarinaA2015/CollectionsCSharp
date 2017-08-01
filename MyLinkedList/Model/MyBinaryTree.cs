88d70a6191ae8d63d7f23ce3e9594e257ff9fc43
Marina <m.abrashkina2015@gmail.com>
2017-08-02 00:43:29 +0300

    Binary Tree was added

---
+++ MyLinkedList/Model/MyBinaryTree.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinkedList.Model
+{
+	class MyBinaryTree<T> : IEnumerable<T>
+	{
+		public NodeBT<T> Root;
+
+		public MyBinaryTree(T data)
+		{
+			NodeBT<T> node = new NodeBT<T>(data);
+			this.Root = node;
+
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			LinkedList<T> forEnumerator = new LinkedList<T>();
+
+			forEnumerator = InOrder(Root, forEnumerator);
+			return forEnumerator.GetEnumerator();
+
+
+		}
+
+		private LinkedList<T> InOrder(NodeBT<T> node, LinkedList<T> enumLL)
+		{
+
+			if (node != null)
+			{
+				InOrder(node.Left, enumLL);
+				enumLL.AddLast(node.Data);
+				InOrder(node.Right, enumLL);
+			}
+			return enumLL;
+		}
+
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		
+	}
+
+}
---
+++ MyLinkedList/Model/NodeBT.cs
@@ -0,0 +1,14 @@
+﻿namespace MyLinkedList.Model
+{
+	class NodeBT<T>
+	{
+		public T Data { get; set; }
+		public NodeBT<T> Left { get; set; }
+		public NodeBT<T> Right { get; set; }
+
+		public NodeBT(T data)
+		{
+			this.Data = data;
+		}
+	}
+}
